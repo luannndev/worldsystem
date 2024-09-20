b1ba5bcc73eb8b2468230b7e6edd348c159f8c48
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-20 20:01:02 +0200

    Add Worlds plugin base class with lifecycle methods
    This commit introduces the base class for the Worlds plugin, which extends JavaPlugin. The class includes essential lifecycle methods onEnable and onDisable, though they are currently empty. This sets up the basic structure needed for future development of the plugin.

---
+++ src/main/java/dev/luan/worlds/Worlds.java
@@ -0,0 +1,18 @@
+package dev.luan.worlds;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public class Worlds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}

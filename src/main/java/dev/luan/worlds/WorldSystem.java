26fbf3e9127a6f15cb11a6c29ef5912024995bbd
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-20 20:15:10 +0200

    Add world management functionality
    Introduced a WorldManager to handle world-related operations, along with a corresponding implementation. Updated the plugin to load worlds on enabling and save worlds on disabling. Added command handling and player join event registration for enhanced interactivity.

--- src/main/java/dev/luan/worlds/WorldSystem.java
+++ src/main/java/dev/luan/worlds/WorldSystem.java
@@ -1,7 +1,11 @@
 package dev.luan.worlds;
 
+import dev.luan.worlds.command.WorldCommand;
 import dev.luan.worlds.config.ConfigManager;
+import dev.luan.worlds.listener.PlayerJoinListener;
 import dev.luan.worlds.message.MessageProvider;
+import dev.luan.worlds.world.WorldManager;
+import dev.luan.worlds.world.WorldManagerImpl;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +15,7 @@ public class WorldSystem extends JavaPlugin {
     private ConfigManager defaultConfig;
     private ConfigManager worldsConfig;
     private MessageProvider messageProvider;
+    private WorldManager worldManager;
 
     @Override
     public void onEnable() {
@@ -20,10 +25,18 @@ public class WorldSystem extends JavaPlugin {
         this.defaultConfig = new ConfigManager(this, "config.yml");
         this.worldsConfig = new ConfigManager(this, "worlds.yml");
         this.messageProvider = new MessageProvider(defaultConfig, configDE, configEN);
+        this.worldManager = new WorldManagerImpl(this, worldsConfig.getConfiguration());
+
+        worldManager.loadWorlds();
+
+        getCommand("world").setExecutor(new WorldCommand(this, worldManager, messageProvider));
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
     }
 
     @Override
     public void onDisable() {
-
+        if (worldManager != null) {
+            worldManager.saveWorlds();
+        }
     }
 }

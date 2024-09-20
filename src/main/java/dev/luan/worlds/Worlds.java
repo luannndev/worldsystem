ed851826685252020426376cfeebb0165c6198c0
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-20 20:01:39 +0200

    Initialize configuration managers
    Added new configuration managers for default settings and world-specific settings in the onEnable method. This includes the default configuration file, world-specific configuration file, and language configuration files (de.yml and en.yml).

--- src/main/java/dev/luan/worlds/Worlds.java
+++ src/main/java/dev/luan/worlds/Worlds.java
@@ -1,14 +1,22 @@
 package dev.luan.worlds;
 
+import dev.luan.worlds.config.ConfigManager;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
 public class Worlds extends JavaPlugin {
 
+    private ConfigManager defaultConfig;
+    private ConfigManager worldsConfig;
+
     @Override
     public void onEnable() {
+        ConfigManager configDE = new ConfigManager(this, "de.yml");
+        ConfigManager configEN = new ConfigManager(this, "en.yml");
 
+        this.defaultConfig = new ConfigManager(this, "config.yml");
+        this.worldsConfig = new ConfigManager(this, "worlds.yml");
     }
 
     @Override
